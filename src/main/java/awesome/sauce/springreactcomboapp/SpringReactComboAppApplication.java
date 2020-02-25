d052a89a00741010ce1fd72262dfe13871a90185
Josh Leong <josh@Joshs-MacBook.local>
2020-02-24 20:01:57 -0700

    initial commit

---
+++ src/main/java/awesome/sauce/springreactcomboapp/SpringReactComboAppApplication.java
@@ -0,0 +1,13 @@
+package awesome.sauce.springreactcomboapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactComboAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactComboAppApplication.class, args);
+	}
+
+}
